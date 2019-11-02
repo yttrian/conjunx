523063d606f5157416cc688164ae23c942a804ac
Ian Moore <privet.privet250@gmail.com>
2019-11-02 16:34:01 -0400

    Define editor endpoints

--- editor/server.py
+++ editor/server.py
@@ -9,11 +9,45 @@ spool = '/var/spool/conjunx'
 
 routes = web.RouteTableDef()
 
+# The user facing editor
+
 @routes.get('/')
 async def hello(request):
     await get_session(request)
 
     return web.Response(text="Hello, world")
+
+# Clip/transcript related endpoints
+
+@routes.post('/clip/')
+async def upload_clip(request):
+    return web.HTTPNotImplemented()
+
+@routes.delete('/clip/{id}')
+async def delete_clip(request):
+    return web.HTTPNotImplemented()
+
+@routes.patch('/clip/{id}/transcript')
+async def update_transcript(request):
+    return web.HTTPNotImplemented()
+
+# .cjxa archive related endpoints
+
+@routes.get('/archive')
+async def download_archive(request):
+    return web.HTTPNotImplemented()
+
+@routes.put('/archive')
+async def upload_archive(request):
+    return web.HTTPNotImplemented()
+
+# Render related endpoints
+
+@routes.get('/render')
+async def render(request):
+    return web.HTTPNotImplemented()
+
+# Gunicorn app builder
 
 async def run():
     app = web.Application()
